7bb4fdd2a1c51fc8559edfb43ac686611f50bd3f
Ayiekoo <131998667+Ayiekoo@users.noreply.github.com>
2023-06-14 02:30:39 +0300

    Add files via upload

---
+++ and, or, not.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+## logical operators
+
+"""
+and, or, not
+
+(x and y): both x and y must be true
+(x or y): either x or y must be true
+(not x) x must be false
+"""
+
+x, y = True, False
+print(x or y)
+
+
+# In[3]:
+
+
+print(x and y)
+
+
+# In[4]:
+
+
+print(not y)
+
+
+# In[ ]:
+
+
+
+
